d08f21e9474fa604ce0067c18e2d8dcc0e10cd12
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2023-07-22 20:36:11 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+
+//This is a function that sets a default value
+function saturdayFun(event = "roller-skate"){
+    return `This Saturday, I want to ${event}!`
+}
+
+
+//This is a function expression
+const mondayWork = function (doSomething = 'go to the office'){
+    return `This Monday, I will ${doSomething}.`
+}
+mondayWork;npm
+
+// IIFE (imediately invoked function expression)
+function wrapAdjective(flair = "*") {
+    return function (adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  // Call example:
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("amazing")); // Output: "You are !!!amazing!!!"
+  console.log(encouragingPromptFunction()); // Output: "You are ***special***"
